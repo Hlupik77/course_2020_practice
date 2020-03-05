a0a70b2e1166385d23d306976cf78c0a718a21ff
Hlupik77 <hlupik77@gmail.com>
2020-03-06 01:05:49 +0300

    Упорядочение свойств объекта

--- object.js
+++ object.js
@@ -33,20 +33,20 @@ myFamily[play] = true; // изменил значение в обьекте
 
 //Свойства переменной
 
-function makeUsers (name, age){
-  return{
+function makeUsers(name, age) {
+  return {
     name: name, // если  названия свойств совпадают с названиеми переменных, то можем написать вместо name: name просто- name,
     age: age,
   };
 }
-let user = makeUsers ('Sasha', 29);
+let user = makeUsers('Sasha', 29);
 console.log(user.name, user.age);
 
 //сокращенно можно так.
 
 let users = {
-name, // тоже самое name: name;
-age: 42
+  name, // тоже самое name: name;
+  age: 42
 };
 
 
@@ -59,41 +59,66 @@ let obj = { // свойства обьекта могутназываться з
   return: 7
 };
 
-console.log( obj.for + obj.let + obj.return );
+console.log(obj.for + obj.let + obj.return);
 
 //Проверка существования свойства, оператор  in
 
 let chekUndefined = {};//если свойства нет возвращается undefined, можно просто проверить сравнением.
 
-console.log(chekUndefined.noSuchProperty === undefined );
+console.log(chekUndefined.noSuchProperty === undefined);
 
 let cat = {
   name: 'Barsik',
   length: 50,
 };
-console.log ('length' in cat);// true, cat.length - существует. Имя свойства обязательно в кавычках, если без них, то мы укажем переменную
-console.log ('blabla' in cat);// false, cat.blabla - не существует
+console.log('length' in cat);// true, cat.length - существует. Имя свойства обязательно в кавычках, если без них, то мы укажем переменную
+console.log('blabla' in cat);// false, cat.blabla - не существует
 
 
 //Цикл for ..... in - используется для перебоки всех свойств обьекта
 // Синтаксис for(key in object) {тело цикла выполняется для каждого свойства обьекта}
 
 let dog = {
-name: "Bobik",
-age: 5,
-color: 'white',
-cable: true,
+  name: "Bobik",
+  age: 5,
+  color: 'white',
+  cable: true,
 };
 
 for (let key in dog) {
-console.log(key); // ключи
-console.log(dog[key]) // значение ключей
+  console.log(key); // ключи
+  console.log(dog[key]) // значение ключей
 
 };
 
 
-for (let prop in myFamily){
-console.log(prop);
-console.log(myFamily[prop]);
-}
+// for (let prop in myFamily){
+// console.log(prop);
+// console.log(myFamily[prop]);
+// }
 
+//Упорядочение свойств объекта
+
+let codes = {
+  "49": "Германия",
+  "41": "Швейцария",
+  "44": "Великобритания",
+  "1": "США",
+};
+
+for (let code in codes) {
+  console.log(code); // 1, 41, 44, 49
+}
+//  "49" свойство целочисленное - выводится по возрастанию
+//"+49" свойство не целочисленное выводится по порядку создания
+let codes_plus = {
+  "+49": "Германия",
+  "+41": "Швейцария",
+  "+44": "Великобритания",
+  // ..,
+  "+1": "США"
+};
+
+for (let code in codes_plus) {
+  console.log( +code ); // 49, 41, 44, 1
+}
